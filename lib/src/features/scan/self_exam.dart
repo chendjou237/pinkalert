9d48d7544937edafcb98da3a24976817460f7f89
baimamboukar <baimamboukar@gmail.com>
2023-03-06 11:02:10 +0100

    configuring widgets

--- lib/src/features/scan/self_exam.dart
+++ lib/src/features/scan/self_exam.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:collectX/src/utils/palette.dart';
-import 'package:collectX/src/utils/text_styles.dart';
 
-class SelfCheck extends ConsumerWidget {
+class SelfCheck extends StatelessWidget {
   const SelfCheck({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     // ignore: avoid_unnecessary_containers
     return Container(
-      child: Center(
+      child: const Center(
         child: Text(
           'SelfCheck',
-          style: TextStyles.designText(
-              bold: true, color: Palette.primary, size: 18),
         ),
       ),
     );
