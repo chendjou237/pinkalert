9832e4e8df826d57141fda2a6597687a4dde372c
baimamboukar <baimamboukar@gmail.com>
2023-03-06 11:02:10 +0100

    configuring widgets

--- lib/src/features/onboarding/welcome/welcome.dart
+++ lib/src/features/onboarding/welcome/welcome.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intellibra/src/features/extensions/build_context.dart';
+import 'package:intellibra/src/extensions/build_context.dart';
 import 'package:intellibra/src/features/onboarding/thememode/cubit/theme_cubit.dart';
 
 class Welcome extends StatelessWidget {
