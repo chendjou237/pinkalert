1728f006c8c02c08e33fdd2bdb584b2b476e97ac
baimamboukar <baimamboukar@gmail.com>
2023-03-06 10:36:29 +0100

    intellibra

---
+++ lib/src/features/auth/domain/exceptions/email_login_exceptions.dart
@@ -0,0 +1,31 @@
+// coverage:ignore-file
+class LogInWithEmailAndPasswordException implements Exception {
+  const LogInWithEmailAndPasswordException([
+    this.message = 'Unknown error occurred.',
+  ]);
+
+  factory LogInWithEmailAndPasswordException.fromCode(String code) {
+    switch (code) {
+      case 'invalid-email':
+        return const LogInWithEmailAndPasswordException(
+          'Email is not valid or badly formatted.',
+        );
+      case 'user-disabled':
+        return const LogInWithEmailAndPasswordException(
+          'This user has been disabled. Please contact support for help.',
+        );
+      case 'user-not-found':
+        return const LogInWithEmailAndPasswordException(
+          'Email is not found, please create an account.',
+        );
+      case 'wrong-password':
+        return const LogInWithEmailAndPasswordException(
+          'Incorrect password, please try again.',
+        );
+      default:
+        return const LogInWithEmailAndPasswordException();
+    }
+  }
+
+  final String message;
+}
