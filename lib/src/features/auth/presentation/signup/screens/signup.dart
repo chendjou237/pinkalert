85cf3ba8c07ffc0b466fadf7838853202009df70
baimamboukar <baimamboukar@gmail.com>
2023-03-06 11:02:10 +0100

    configuring widgets

--- lib/src/features/auth/presentation/signup/screens/signup.dart
+++ lib/src/features/auth/presentation/signup/screens/signup.dart
@@ -1,26 +1,25 @@
-import 'package:akila/src/app/extensions/gap.dart';
-import 'package:akila/src/app/extensions/text_style.dart';
-import 'package:akila/src/app/extensions/theme.dart';
-import 'package:akila/src/features/auth/widgets/akila_button.dart';
-import 'package:akila/src/features/auth/widgets/akila_input.dart';
-import 'package:akila/src/features/common/widgets/logo.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hicons/flutter_hicons.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intellibra/src/common/common.dart';
+import 'package:intellibra/src/common/widgets/akila_input.dart';
+import 'package:intellibra/src/extensions/build_context.dart';
+import 'package:intellibra/src/extensions/num.dart';
 import 'package:pinput/pinput.dart';
 
-class CreateAccount extends ConsumerStatefulWidget {
+class CreateAccount extends StatefulWidget {
   const CreateAccount({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _CreateAccountState();
+  State<CreateAccount> createState() => _CreateAccountState();
 }
 
-class _CreateAccountState extends ConsumerState<CreateAccount> {
-  final PageController _pageController = PageController(initialPage: 0);
+class _CreateAccountState extends State<CreateAccount> {
+  final PageController _pageController = PageController();
+
   int _currentPageIndex = 0;
+
   bool _isLastPage = false;
 
   @override
@@ -58,21 +57,22 @@ class _CreateAccountState extends ConsumerState<CreateAccount> {
                 ],
               ),
             ),
-            AkilaButton(
+            IntellibraButton(
               action: _isLastPage ? _submitForm : _nextPage,
               text: _isLastPage ? 'Valider' : 'Continuer',
             ),
-            _currentPageIndex != 0
-                ? TextButton(
-                    onPressed: () {
-                      _pageController.previousPage(
-                        duration: const Duration(milliseconds: 500),
-                        curve: Curves.easeInOut,
-                      );
-                    },
-                    child: const Text('Retour'),
-                  )
-                : const SizedBox.shrink(),
+            if (_currentPageIndex != 0)
+              TextButton(
+                onPressed: () {
+                  _pageController.previousPage(
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.easeInOut,
+                  );
+                },
+                child: const Text('Retour'),
+              )
+            else
+              const SizedBox.shrink(),
             24.vGap
           ],
         ),
@@ -88,7 +88,7 @@ class _CreateAccountState extends ConsumerState<CreateAccount> {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Logo(),
+            const FlutterLogo(),
             const Spacer(),
             // Dropdown menu for language
             Expanded(
@@ -96,19 +96,21 @@ class _CreateAccountState extends ConsumerState<CreateAccount> {
                 // leadingIcon: const Icon(Hicons.work_bold),
                 // menuStyle: MenuStyle(),
                 // trailingIcon: const Icon(Hicons.arrow_swap_vertical),
-                value: "Francais",
+                value: 'Francais',
 
                 decoration: const InputDecoration(
                   //  prefixIcon: Icon(Icons.language),
                   border: InputBorder.none,
                   contentPadding: EdgeInsets.all(0),
-                  hintText: "Francais",
+                  hintText: 'Francais',
                 ),
                 onChanged: (language) {},
-                items: ["English", "Francais"]
+                items: ['English', 'Francais']
                     .map(
                       (lang) => DropdownMenuItem<String>(
-                          value: lang, child: Text(lang)),
+                        value: lang,
+                        child: Text(lang),
+                      ),
                     )
                     .toList(),
               ),
@@ -117,19 +119,19 @@ class _CreateAccountState extends ConsumerState<CreateAccount> {
         ),
         14.vGap,
         Stack(
-          fit: StackFit.loose,
           clipBehavior: Clip.none,
           children: [
             Container(
               height: 100,
               width: 100,
               decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: context.colorScheme.secondary.withOpacity(.5)),
+                shape: BoxShape.circle,
+                color: context.scheme.secondary.withOpacity(.5),
+              ),
               child: Icon(
                 Hicons.profile_circle,
                 size: 85,
-                color: context.colorScheme.onPrimary.withOpacity(.75),
+                color: context.scheme.onPrimary.withOpacity(.75),
               ),
             ),
             Positioned(
@@ -138,7 +140,7 @@ class _CreateAccountState extends ConsumerState<CreateAccount> {
               child: Icon(
                 Icons.add_a_photo_outlined,
                 size: 24,
-                color: context.colorScheme.secondary.withOpacity(.75),
+                color: context.scheme.secondary.withOpacity(.75),
               ),
             ),
             // Center(child: Image.asset("assets/icons/bg.png")),
@@ -158,19 +160,20 @@ class _CreateAccountState extends ConsumerState<CreateAccount> {
         const Padding(
           padding: EdgeInsets.symmetric(horizontal: 34),
           child: Text(
-              "Ajouter votre photo de profil ou le logo de votre entreprise"),
+            'Ajouter votre photo de profil ou le logo de votre entreprise',
+          ),
         ),
         14.vGap,
         Center(
           child: Text(
-            "Entrez votre vrai Nom de Naissance",
-            style: context.theme.textTheme.bodyLarge!.bold,
+            'Entrez votre vrai Nom de Naissance',
+            style: context.theme.textTheme.bodyLarge,
           ),
         ),
         //  4.vGap,
-        AkilaInput(
-          label: "Noms de naissance",
-          hint: "Lady Bug Miraculous",
+        IntelliInput(
+          label: 'Noms de naissance',
+          hint: 'Lady Bug Miraculous',
           controller: TextEditingController(),
           obscureText: false,
           keyboardType: TextInputType.number,
@@ -188,7 +191,7 @@ class _CreateAccountState extends ConsumerState<CreateAccount> {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Logo(),
+            const FlutterLogo(),
             const Spacer(),
             // Dropdown menu for language
             Expanded(
@@ -196,19 +199,21 @@ class _CreateAccountState extends ConsumerState<CreateAccount> {
                 // leadingIcon: const Icon(Hicons.work_bold),
                 // menuStyle: MenuStyle(),
                 // trailingIcon: const Icon(Hicons.arrow_swap_vertical),
-                value: "Francais",
+                value: 'Francais',
 
                 decoration: const InputDecoration(
                   //  prefixIcon: Icon(Icons.language),
                   border: InputBorder.none,
                   contentPadding: EdgeInsets.all(0),
-                  hintText: "Francais",
+                  hintText: 'Francais',
                 ),
                 onChanged: (language) {},
-                items: ["English", "Francais"]
+                items: ['English', 'Francais']
                     .map(
                       (lang) => DropdownMenuItem<String>(
-                          value: lang, child: Text(lang)),
+                        value: lang,
+                        child: Text(lang),
+                      ),
                     )
                     .toList(),
               ),
@@ -220,32 +225,30 @@ class _CreateAccountState extends ConsumerState<CreateAccount> {
           child: Icon(
             Hicons.lock_1,
             size: 64,
-            color: context.colorScheme.primary,
+            color: context.scheme.primary,
           ),
         ),
         4.vGap,
         const Center(
           child: Text(
-            "Entrez votre code secret",
+            'Entrez votre code secret',
             style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
           ),
         ),
         14.vGap,
         const Center(
-          child: Pinput(
-            length: 4,
-          ),
+          child: Pinput(),
         ),
         14.vGap,
         ListTile(
           horizontalTitleGap: 0,
           title: Text(
-            "Utilisez votre empreinte comme mode de déverrouillage",
+            'Utilisez votre empreinte comme mode de déverrouillage',
             style: context.theme.textTheme.bodySmall,
           ),
           leading: const Icon(Icons.fingerprint),
           trailing: CupertinoSwitch(
-            activeColor: context.colorScheme.primary,
+            activeColor: context.scheme.primary,
             value: true,
             onChanged: (fingerprintAuth) {},
           ),
@@ -263,7 +266,7 @@ class _CreateAccountState extends ConsumerState<CreateAccount> {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Logo(),
+            const FlutterLogo(),
             const Spacer(),
             // Dropdown menu for language
             Expanded(
@@ -271,19 +274,21 @@ class _CreateAccountState extends ConsumerState<CreateAccount> {
                 // leadingIcon: const Icon(Hicons.work_bold),
                 // menuStyle: MenuStyle(),
                 // trailingIcon: const Icon(Hicons.arrow_swap_vertical),
-                value: "Francais",
+                value: 'Francais',
 
                 decoration: const InputDecoration(
                   //  prefixIcon: Icon(Icons.language),
                   border: InputBorder.none,
                   contentPadding: EdgeInsets.all(0),
-                  hintText: "Francais",
+                  hintText: 'Francais',
                 ),
                 onChanged: (language) {},
-                items: ["English", "Francais"]
+                items: ['English', 'Francais']
                     .map(
                       (lang) => DropdownMenuItem<String>(
-                          value: lang, child: Text(lang)),
+                        value: lang,
+                        child: Text(lang),
+                      ),
                     )
                     .toList(),
               ),
@@ -295,32 +300,30 @@ class _CreateAccountState extends ConsumerState<CreateAccount> {
           child: Icon(
             Hicons.lock_1,
             size: 64,
-            color: context.colorScheme.primary,
+            color: context.scheme.primary,
           ),
         ),
         4.vGap,
         const Center(
           child: Text(
-            "Verifiez votre code secret",
+            'Verifiez votre code secret',
             style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
           ),
         ),
         14.vGap,
         const Center(
-          child: Pinput(
-            length: 4,
-          ),
+          child: Pinput(),
         ),
         14.vGap,
         ListTile(
           horizontalTitleGap: 0,
           title: Text(
-            "Utilisez votre empreinte comme mode de déverrouillage",
+            'Utilisez votre empreinte comme mode de déverrouillage',
             style: context.theme.textTheme.bodySmall,
           ),
           leading: const Icon(Icons.fingerprint),
           trailing: CupertinoSwitch(
-            activeColor: context.colorScheme.primary,
+            activeColor: context.scheme.primary,
             value: true,
             onChanged: (fingerprintAuth) {},
           ),
@@ -340,143 +343,3 @@ class _CreateAccountState extends ConsumerState<CreateAccount> {
     context.router.pushNamed('/home');
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import 'package:akila/src/app/extensions/texttheme.dart';
-// import 'package:akila/src/app/extensions/theme.dart';
-// import 'package:akila/src/features/auth/widgets/akila_button.dart';
-// import 'package:akila/src/features/auth/widgets/akila_input.dart';
-// import 'package:akila/src/features/common/widgets/logo.dart';
-// import 'package:auto_route/auto_route.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-// class Signup extends ConsumerStatefulWidget {
-//   const Signup({super.key});
-//   @override
-//   ConsumerState<ConsumerStatefulWidget> createState() => _SignupState();
-// }
-
-// class _SignupState extends ConsumerState<Signup> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(
-//       child: Scaffold(
-//           body: SingleChildScrollView(
-//         child: Padding(
-//           padding: const EdgeInsets.symmetric(horizontal: 14.0),
-//           child: Column(
-//             children: [
-//               Row(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   const Logo(),
-//                   const Spacer(),
-//                   // Dropdown menu for language
-//                   DropdownMenu<String>(
-//                     // leadingIcon: const Icon(Hicons.work_bold),
-//                     // menuStyle: MenuStyle(),
-//                     // trailingIcon: const Icon(Hicons.arrow_swap_vertical),
-//                     enableSearch: false,
-//                     width: 50,
-//                     inputDecorationTheme:
-//                         const InputDecorationTheme(border: InputBorder.none),
-//                     textStyle: context.bodytext
-//                         .copyWith(color: context.colorScheme.primary),
-//                     initialSelection: "English",
-//                     onSelected: (language) {},
-//                     dropdownMenuEntries: ["English", "Francais"]
-//                         .map(
-//                           (lang) => DropdownMenuEntry<String>(
-//                               label: lang, value: lang),
-//                         )
-//                         .toList(),
-//                   )
-//                 ],
-//               ),
-//               const SizedBox(height: 14),
-//               Text(
-//                 "Creer un Compte",
-//                 style: context.bodylarge,
-//               ),
-//               const SizedBox(height: 14),
-//               Stack(
-//                 fit: StackFit.loose,
-//                 children: [
-//                   Center(child: Image.asset("assets/icons/bg.png")),
-//                   Center(
-//                     child: Image.asset(
-//                       "assets/icons/add-user-image.png",
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//               AkilaInput(
-//                 label: "noms complet",
-//                 hint: "Lady Bug Miraculous",
-//                 controller: TextEditingController(),
-//                 obscureText: false,
-//                 keyboardType: TextInputType.name,
-//               ),
-//               AkilaInput(
-//                 label: "Numero",
-//                 hint: "690535759",
-//                 controller: TextEditingController(),
-//                 obscureText: false,
-//                 prefix: Image.asset("assets/icons/flag.png"),
-//                 keyboardType: TextInputType.number,
-//               ),
-//               AkilaInput(
-//                 label: "Adresse email",
-//                 hint: "lady-bug@gmail.com",
-//                 controller: TextEditingController(),
-//                 obscureText: false,
-//                 keyboardType: TextInputType.emailAddress,
-//               ),
-//               AkilaInput(
-//                 label: "Code secret",
-//                 hint: "8113",
-//                 controller: TextEditingController(),
-//                 obscureText: true,
-//                 keyboardType: TextInputType.number,
-//               ),
-//               const SizedBox(
-//                 height: 24,
-//               ),
-//               AkilaButton(
-//                 text: "Creer un compte",
-//                 action: () {
-//                   context.router.pushNamed('/home');
-//                 },
-//               )
-//             ],
-//           ),
-//         ),
-//       )),
-//     );
-//   }
-// }
-
