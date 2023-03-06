c7b3cb61db9170c8c358cafc1f62b1fc9462a291
baimamboukar <baimamboukar@gmail.com>
2023-03-06 10:36:28 +0100

    intellibra

---
+++ lib/src/common/widgets/intellibra_button.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:intellibra/src/configs/palette.dart';
+import 'package:intellibra/src/configs/intellibra_constants.dart';
+import 'package:intellibra/src/features/extensions/build_context.dart';
+import 'package:intellibra/src/features/extensions/num.dart';
+
+class IntellibraButton extends StatelessWidget {
+  const IntellibraButton({
+    required this.text,
+    this.action,
+    this.color = Palette.primary,
+    super.key,
+    this.width = .9,
+    this.icon,
+  });
+  final IconData? icon;
+  final String text;
+  final VoidCallback? action;
+  final Color color;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: action,
+      child: Container(
+        width: context.width * width,
+        height: Constants.buttonHeight,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(Constants.borderRadius),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: context.bodyLg.copyWith(
+                color: context.scheme.onPrimary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            if (icon != null) ...[
+              8.hGap,
+              Icon(icon),
+            ] else
+              const SizedBox.shrink(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class IntellibraButtonMedium extends StatelessWidget {
+  const IntellibraButtonMedium({
+    required this.text,
+    this.action,
+    this.color = Palette.primary,
+    super.key,
+    this.width = .35,
+    this.icon,
+  });
+  final IconData? icon;
+  final String text;
+  final VoidCallback? action;
+  final Color color;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return IntellibraButton(
+      text: text,
+      action: action,
+      color: color,
+      width: width,
+      icon: icon,
+    );
+  }
+}
