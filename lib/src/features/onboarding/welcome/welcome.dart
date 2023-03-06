385c27efa6a553a94d92c9af97323877111ae430
baimamboukar <baimamboukar@gmail.com>
2023-03-06 11:25:20 +0100

    configured entry flow

--- lib/src/features/onboarding/welcome/welcome.dart
+++ lib/src/features/onboarding/welcome/welcome.dart
@@ -1,6 +1,8 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intellibra/src/common/common.dart';
 import 'package:intellibra/src/extensions/build_context.dart';
 import 'package:intellibra/src/features/onboarding/thememode/cubit/theme_cubit.dart';
 
@@ -27,13 +29,86 @@ class Welcome extends StatelessWidget {
               ),
             ],
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(24),
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+          body: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.5,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomRight,
+                    colors: [
+                      context.scheme.primary.withOpacity(.7),
+                      context.scheme.primary.withOpacity(.2),
+                    ],
+                  ),
+                  // image: const DecorationImage(
+                  //   image: AssetImage(Assets.assetsIconsAppIcon),
+                  //   fit: BoxFit.cover,
+                  // ),
+                ),
               ),
-            ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: context.theme.primaryColor,
+                    gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      colors: [
+                        context.scheme.primary.withOpacity(.7),
+                        context.scheme.primary.withOpacity(.8),
+                        context.scheme.primary.withOpacity(.9),
+                        context.scheme.primary.withOpacity(1),
+                      ],
+                    ),
+                  ),
+                  width: double.infinity,
+                  child: Column(
+                    children: [
+                      Text(
+                        'Welcome to Intellibra',
+                        style: context.theme.textTheme.displaySmall!.copyWith(
+                          color: context.scheme.onPrimary,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 120),
+                        child: Text(
+                          'Scan hoha',
+                          style: context.theme.textTheme.bodyMedium!.copyWith(
+                            color: context.scheme.onBackground,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      const Spacer(),
+                      IntellibraButton(
+                        text: 'Take to Akila',
+                        color: context.scheme.primary,
+                        action: () {
+                          context.router.pushNamed('/home');
+                        },
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          context.router.pushNamed('/signup');
+                        },
+                        // icon: const Icon(Icons.add),
+                        child: Text(
+                          'Sign Up',
+                          style:
+                              context.theme.textTheme.headlineSmall!.copyWith(
+                            fontSize: 14,
+                            color: context.scheme.onPrimary,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+              )
+            ],
           ),
         );
       },
