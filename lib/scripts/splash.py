6394ec386745f97785030939d71e56648d1fde0a
baimamboukar <baimamboukar@gmail.com>
2023-03-06 10:36:27 +0100

    intellibra

---
+++ lib/scripts/splash.py
@@ -0,0 +1,6 @@
+import os
+
+# execute command
+os.system("----Generating Splash Screen----")
+os.system("flutter pub run flutter_launcher_icons:main")
+os.system("flutter pub run flutter_native_splash:create")
