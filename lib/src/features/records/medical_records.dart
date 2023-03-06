a434fde8ea9df3d79ac34b70b706607511aaac54
baimamboukar <baimamboukar@gmail.com>
2023-03-06 11:02:10 +0100

    configuring widgets

--- lib/src/features/records/medical_records.dart
+++ lib/src/features/records/medical_records.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:collectX/src/utils/palette.dart';
-import 'package:collectX/src/utils/text_styles.dart';
 
-class Records extends ConsumerWidget {
-  const Records({super.key});
+class IntelliRecords extends StatelessWidget {
+  const IntelliRecords({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     // ignore: avoid_unnecessary_containers
     return Container(
-      child: Center(
+      child: const Center(
         child: Text(
           'Records',
-          style: TextStyles.designText(
-              bold: true, color: Palette.primary, size: 18),
         ),
       ),
     );
