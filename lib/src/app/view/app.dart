a0a26941e59e91d607cd37622fb135c051a1072b
baimamboukar <baimamboukar@gmail.com>
2023-03-06 10:36:28 +0100

    intellibra

---
+++ lib/src/app/view/app.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intellibra/l10n/l10n.dart';
+import 'package:intellibra/src/configs/intellibra_theme.dart';
+import 'package:intellibra/src/features/onboarding/thememode/cubit/theme_cubit.dart';
+import 'package:intellibra/src/router/intellibra_router.gr.dart';
+
+class Intellibra extends StatefulWidget {
+  const Intellibra({super.key});
+
+  @override
+  State<Intellibra> createState() => _IntellibraState();
+}
+
+class _IntellibraState extends State<Intellibra> {
+  late IntellibraRouter router;
+  @override
+  void initState() {
+    router = IntellibraRouter();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const theme = IntellibraTheme();
+    return BlocBuilder<ThemeCubit, bool>(
+      builder: (BuildContext context, bool state) {
+        return MaterialApp.router(
+          themeMode: state == false ? ThemeMode.light : ThemeMode.dark,
+          theme: theme.toThemeData(
+            Brightness.light,
+          ),
+          darkTheme: theme.toThemeData(
+            Brightness.dark,
+          ),
+          routeInformationParser: router.defaultRouteParser(),
+          routerDelegate: router.delegate(),
+          supportedLocales: AppLocalizations.supportedLocales,
+        );
+      },
+    );
+  }
+}
