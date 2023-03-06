1e6adfe62712be508cacb9ce13bf65345a733629
baimamboukar <baimamboukar@gmail.com>
2023-03-06 10:36:30 +0100

    intellibra

---
+++ lib/src/features/onboarding/thememode/choose_theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChooseTheme extends StatelessWidget {
+  const ChooseTheme({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Choose your theme mode'),
+      ),
+    );
+  }
+}
