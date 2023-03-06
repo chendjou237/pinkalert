0a2c188bac266ea15dd60d5641f0e3d0998275fe
baimamboukar <baimamboukar@gmail.com>
2023-03-06 10:36:30 +0100

    intellibra

---
+++ lib/src/features/onboarding/language/choose_language.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChooseLanguage extends StatelessWidget {
+  const ChooseLanguage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Choose your preferred language'),
+      ),
+    );
+  }
+}
