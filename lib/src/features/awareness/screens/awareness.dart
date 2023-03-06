16cf0f439200305673a3b52111a4ed22f179c4a3
baimamboukar <baimamboukar@gmail.com>
2023-03-06 11:09:43 +0100

    configuring widgets

---
+++ lib/src/features/awareness/screens/awareness.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Awareness extends StatelessWidget {
+  const Awareness({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Aweress'),
+    );
+  }
+}
