0c2e176235b61bc1270a10c43329b78c981286d0
baimamboukar <baimamboukar@gmail.com>
2023-03-06 11:02:10 +0100

    configuring widgets

--- lib/src/features/auth/presentation/login/screens/login.dart
+++ lib/src/features/auth/presentation/login/screens/login.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Login extends ConsumerStatefulWidget {
+class Login extends StatelessWidget {
   const Login({super.key});
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _LoginState();
-}
-
-class _LoginState extends ConsumerState<Login> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text("Login")),
+      body: Center(child: Text('Login')),
     );
   }
 }
