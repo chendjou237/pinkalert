24199bcea92d95f96e6707f9024e3d0a6f136565
baimamboukar <baimamboukar@gmail.com>
2023-03-06 11:25:19 +0100

    configured entry flow

---
+++ lib/src/app/assets.dart
@@ -0,0 +1,15 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsIconsAppIcon
+  /// assets/icons/app-icon.png
+  static const String assetsIconsAppIcon = "assets/icons/app-icon.png";
+
+  /// Assets for assetsIconsWoman
+  /// assets/icons/woman.jpg
+  static const String assetsIconsWoman = "assets/icons/woman.jpg";
+
+  /// Assets for assetsIconsWomanPp
+  /// assets/icons/woman_pp.png
+  static const String assetsIconsWomanPp = "assets/icons/woman_pp.png";
+}
