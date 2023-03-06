25d876280e46e2512b9d830fbae17074a78d3bad
baimamboukar <baimamboukar@gmail.com>
2023-03-06 10:36:28 +0100

    intellibra

---
+++ lib/src/configs/intellibra_constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static const String appName = 'Intellibra';
+  static const String appVersion = '1.0.0';
+  static const String appDescription = 'Intellibra';
+
+  static const double borderRadius = 14;
+  static const double buttonHeight = 52;
+}
