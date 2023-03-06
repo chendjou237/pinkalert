d729cf9526fdac1a8ba7085388d831e2472dc06d
baimamboukar <baimamboukar@gmail.com>
2023-03-06 11:25:20 +0100

    configured entry flow

--- lib/src/router/intellibra_router.dart
+++ lib/src/router/intellibra_router.dart
@@ -1,6 +1,6 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:intellibra/src/features/auth/presentation/signup/screens/signup.dart';
 import 'package:intellibra/src/features/awareness/screens/awareness.dart';
-
 import 'package:intellibra/src/features/onboarding/onboarding.dart';
 import 'package:intellibra/src/features/records/medical_records.dart';
 import 'package:intellibra/src/features/scan/home.dart';
@@ -13,6 +13,11 @@ import 'package:intellibra/src/features/scan/self_exam.dart';
       page: Welcome,
       initial: true,
       path: '/welcome',
+    ),
+    AutoRoute(
+      page: CreateAccount,
+      initial: true,
+      path: '/create-account',
     ),
     AutoRoute(
       page: ChooseLanguage,
