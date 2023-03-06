767f83512aba8ef8accaa13990f96485485c70e7
baimamboukar <baimamboukar@gmail.com>
2023-03-06 10:36:31 +0100

    intellibra

---
+++ lib/src/features/onboarding/welcome/welcome.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intellibra/src/features/extensions/build_context.dart';
+import 'package:intellibra/src/features/onboarding/thememode/cubit/theme_cubit.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ThemeCubit, bool>(
+      builder: (BuildContext context, bool state) {
+        return Scaffold(
+          //backgroundColor: context.scheme.background,
+          appBar: AppBar(
+            title: const Text('Intellibra Widgets'),
+            actions: [
+              CupertinoSwitch(
+                value: state,
+                activeColor: context.scheme.tertiary,
+                onChanged: (theme) {
+                  state == true
+                      ? context.read<ThemeCubit>().reset()
+                      : context.read<ThemeCubit>().toggle();
+                },
+              ),
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(24),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
