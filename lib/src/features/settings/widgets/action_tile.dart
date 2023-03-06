4368b0a030a72fd2708007bd6b12830410506961
baimamboukar <baimamboukar@gmail.com>
2023-03-06 11:02:10 +0100

    configuring widgets

---
+++ lib/src/features/settings/widgets/action_tile.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ActionLabel extends StatelessWidget {
+  const ActionLabel({
+    required this.label,
+    required this.onTap,
+    required this.icon,
+    super.key,
+  });
+  final String label;
+  final VoidCallback onTap;
+  final IconData icon;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        radius: 18,
+        child: Icon(icon),
+      ),
+      title: Text(label),
+      onTap: onTap,
+      trailing: const Icon(
+        Icons.chevron_right,
+      ),
+    );
+  }
+}
