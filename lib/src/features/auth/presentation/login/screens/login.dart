195d14190dec4fa1f10571af0b786efda71d97e9
baimamboukar <baimamboukar@gmail.com>
2023-03-06 10:36:29 +0100

    intellibra

---
+++ lib/src/features/auth/presentation/login/screens/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Login extends ConsumerStatefulWidget {
+  const Login({super.key});
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _LoginState();
+}
+
+class _LoginState extends ConsumerState<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("Login")),
+    );
+  }
+}
