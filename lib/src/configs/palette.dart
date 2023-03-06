7c30961c554596a2e136ba63ca8d7c4b29e42232
baimamboukar <baimamboukar@gmail.com>
2023-03-06 10:36:29 +0100

    intellibra

---
+++ lib/src/configs/palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color primary = Color(0xFF0D47A1);
+  static const Color primaryColorDark = Color(0xFF002171);
+  static const Color primaryColorLight = Color(0xFF5472D3);
+  static const Color accentColor = Color(0xFF00E676);
+  static const Color primaryTextColor = Color(0xFF212121);
+  static const Color secondaryTextColor = Color(0xFF757575);
+  static const Color dividerColor = Color(0xFFBDBDBD);
+}
